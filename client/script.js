7ac728dca4d387dad2234a174ada01282ba19093
roshray <roshanroy759@gmail.com>
2022-12-24 16:22:14 +0530

    user interface:✅

--- client/script.js
+++ client/script.js
@@ -38,8 +38,70 @@ function generateUniqueId() {
   const randomNumber = Math.random()
   const hexadecimalString = randomNumber.toString(16)
 
-  return `id-${timestamp}`-${hexadecimalString}
+  return `id-${timestamp}-${hexadecimalString}`
 }
+
+function chatStripe (isAi, value, uniqueId) {
+  return (
+    `
+    <div class="wrapper ${isAi && 'ai'}">
+      <div class="chat">
+        <div class="profile">
+          <img src="${isAi ? bot: user}" alt="${isAi ? 'bot': 'user'}"
+          />
+        </div>
+        <div class="message" id=${uniqueId}>
+        ${value}</div>
+      </div>
+    
+    </div>
+    
+    `
+  )
+}
+
+const handleSubmit = async (e) => {
+  e.preventDefault()
+
+  const data = new FormData(form)
+
+  // user chatStripe
+
+  chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
+
+  form.reset()
+
+  // bot's chatStripe
+
+  const uniqueId = generateUniqueId()
+
+  chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
+
+  chatContainer.scrollTop = chatContainer.scrollHeight
+
+  const messageDiv = document.getElementById(uniqueId)
+
+  loader(messageDiv)
+}
+
+form.addEventListener('submit', handleSubmit)
+form.addEventListener('keyup', (e) => {
+  if(e.keyCode === 13) {
+    handleSubmit(e)
+  }
+})
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
